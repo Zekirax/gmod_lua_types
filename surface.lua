b38f1192cb17d9267abb02f78d3c76b3e85397af
game-1 à bk <tillsanta1034@gmail.com>
2023-10-12 18:27:41 +0200

    add surface

---
+++ surface.lua
@@ -0,0 +1,9 @@
+---@class Gmod_surface
+---@field DrawPoly fun(points : Gmod_surface_draw_poly_point[])
+
+---@class Gmod_surface
+surface = {}
+
+---@class Gmod_surface_draw_poly_point
+---@field x number
+---@field y number
